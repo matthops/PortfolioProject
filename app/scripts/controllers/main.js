4c92b55c0ac307c6311414745564f79587fb2547
Matt Hopkins <matthops@gmail.com>
2014-04-15 21:48:48 -0600

    scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('portfolioProjectApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
